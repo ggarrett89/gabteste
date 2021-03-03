d917a049ddd7393e4c578bb700e7774499bc1294
ggarrett89 <75688051+ggarrett89@users.noreply.github.com>
2021-03-03 15:46:07 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        region = "us-east-1"
-        bucket = "k8s-backend-video"
+        region = "us-east-2"
+        bucket = "bucket-k8s-devops"
         encrypt = "true"
         key = "terraform.tfstate"
     }
